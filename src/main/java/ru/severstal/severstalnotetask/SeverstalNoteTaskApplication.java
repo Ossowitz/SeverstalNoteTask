797ca221bb42c3198b78d483aada4c9602b74ab1
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-06 20:54:08 +0300

    Create empty repository

---
+++ src/main/java/ru/severstal/severstalnotetask/SeverstalNoteTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.severstal.severstalnotetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverstalNoteTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeverstalNoteTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/severstal/severstalnotetask/SeverstalNoteTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.severstal.severstalnotetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverstalNoteTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
