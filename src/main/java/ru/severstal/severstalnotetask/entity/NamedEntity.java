3bb2fa0360eff1d7326fa3a780f03d8c37ba5c56
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-07 01:36:33 +0300

    Add MappedSuperClasses
    • Create BaseEntity and NamedEntity

---
+++ src/main/java/ru/severstal/severstalnotetask/entity/NamedEntity.java
@@ -0,0 +1,30 @@
+package ru.severstal.severstalnotetask.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class NamedEntity extends BaseEntity {
+
+    @NotBlank
+    @Column(name = "name", nullable = false)
+    protected String name;
+
+    protected NamedEntity(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + '[' + name + ']';
+    }
+}
